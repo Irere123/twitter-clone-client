c6ec4646feccc4b3d6fd0d0b3b35005c553041c4
irere123 <irere2020@outlook.com>
2021-09-15 13:10:30 +0200

    feat: added loginto the login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button, Container, Form, Header } from "semantic-ui-react";
 import { Field, Formik } from "formik";
+
 import InputField from "../components/fields/InputField";
+import { LoginComponent } from "../generated/graphql";
 
 interface FormValues {
   username: string;
@@ -14,27 +16,43 @@ const Login: React.FC = () => {
       <Header as="h2" color="teal">
         Login into your account
       </Header>
-      <Formik<FormValues>
-        initialValues={{ username: "", password: "" }}
-        onSubmit={() => {}}
-      >
-        <Form>
-          <Field
-            name="username"
-            label="Username"
-            placeholder="Username"
-            component={InputField}
-          />
-          <Field
-            name="password"
-            label="Password"
-            placeholder="Password"
-            type="password"
-            component={InputField}
-          />
-          <Button color="green">Login</Button>
-        </Form>
-      </Formik>
+      <LoginComponent>
+        {(login) => (
+          <Formik<FormValues>
+            initialValues={{ username: "", password: "" }}
+            onSubmit={async (input) => {
+              const response = await login({
+                variables: { input },
+              });
+
+              console.log(response);
+            }}
+          >
+            {({ values, handleChange, handleSubmit, isSubmitting }) => (
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  value={values.username}
+                  name="username"
+                  onChange={handleChange}
+                  placeholder="Username"
+                  component={InputField}
+                />
+                <Field
+                  value={values.password}
+                  name="password"
+                  onChange={handleChange}
+                  placeholder="Password"
+                  type="password"
+                  component={InputField}
+                />
+                <Button disabled={isSubmitting} color="green">
+                  Login
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        )}
+      </LoginComponent>
     </Container>
   );
 };
